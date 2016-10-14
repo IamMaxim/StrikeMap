814f75860484ab8cf6943f4f732a9af2f725c4c4
IamMaxim <g.maxim.stepanoff@gmail.com>
2016-10-14 23:10:41 +0300

    Modified server

--- src/main/java/ru/strikemap/ServerThread.java
+++ src/main/java/ru/strikemap/ServerThread.java
@@ -1,18 +1,31 @@
 package ru.strikemap;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by maxim on 14.10.2016.
  */
 public class ServerThread extends Thread {
-    public final ArrayList<Player> players = new ArrayList<>();
+    private final HashMap<Integer, Player> players = new HashMap<>();
+
+    public void add(Player player) {
+        synchronized (players) {
+            players.put(player.id, player);
+        }
+    }
+
+    public void remove(int id) {
+        synchronized (players) {
+            players.remove(id);
+        }
+    }
 
     @Override
     public void run() {
         while (!isInterrupted()) {
             synchronized (players) {
-                players.forEach(Player::update);
+                players.values().forEach(Player::update);
             }
         }
     }
