a375cc466c147fc4e3a322d42662acb30ef07ea5
Gleb Nazemnov <nazemnovgleb@gmail.com>
2016-10-14 23:05:37 +0300

    player class inicialization

---
+++ src/main/java/ru/strikemap/player.java
@@ -0,0 +1,25 @@
+package ru.strikemap;
+
+/**
+ * Created by glebnazemnov on 14.10.16.
+ */
+
+//Каркас класса для игрока
+
+public class player {
+    private String namePlayer;
+    private String team;
+    private int xcoord, ycoord;
+    public static boolean isAlive(){
+        return false;
+    }
+    public static boolean isHaveAmmo(){
+        return false;
+    }
+    public void setName(String name){
+        this.namePlayer = name;
+    }
+    public void setTeam(String teamname){
+        this.team = teamname;
+    }
+}
